6965f510df7ac322a11ef40d12c9af677ba101a6
Albert Nikolli <albert_nikolli@icloud.com>
2024-10-31 19:11:56 +0100

    worked some of the specific pages and created the connections of the links

--- app/javascript/controllers/burger_menu_controller.js
+++ app/javascript/controllers/burger_menu_controller.js
@@ -6,18 +6,21 @@ export default class extends Controller {
     const burgerMenu = document.getElementById('burger-menu');
     const menu = document.getElementById('menu-container');
     const julian = document.getElementById('logo-text');
-    const textInfo = document.getElementById('text-info');
+    const textInfo1Pc = document.getElementById('text-info-1-pc');
+    const textInfo1Phone = document.getElementById('text-info-1-phone');
 
     burgerMenu.addEventListener('click', () => {
       burgerMenu.classList.toggle('active');
       if (burgerMenu.classList.contains('active')) {
         menu.style = 'opacity: 1;z-index:1000';
         julian.style = 'display: none'
-        textInfo.style = 'display: none'
+        textInfo1Pc.style = 'display: none'
+        textInfo1Phone.style = 'display: none'
       } else {
         menu.style = 'opacity: 0; z-index:0';
         julian.style = 'display: inline-block'
-        textInfo.style = 'display: inline-block'
+        textInfo1Pc.style = 'display: inline-block'
+        textInfo1Phone.style = 'display: inline-block'
       }
     });
   }
