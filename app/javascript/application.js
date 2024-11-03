52d1bca5320ba88cb621dee4552807337ea410a6
Albert Nikolli <albert_nikolli@icloud.com>
2024-11-03 13:12:36 +0100

    splash page changes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,12 +6,12 @@ import "bootstrap"
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  // if (window.location.href == "https://www.juliannicosia.com/" ) {
-    if (window.location.href == "http://localhost:3000/" ) {
+  if (window.location.href == "https://www.juliannicosia.com/" ) {
+    // if (window.location.href == "http://localhost:3000/" ) {
     setTimeout(function() {
       // Redirect to the main content after 4.3 seconds
-      // window.location.href = "https://www.juliannicosia.com/home";
-      window.location.href = "http://localhost:3000/home";
+      window.location.href = "https://www.juliannicosia.com/home";
+      // window.location.href = "http://localhost:3000/home";
     }, 4300); // 4300 milliseconds = 4.3 seconds
   } else {
     ''
