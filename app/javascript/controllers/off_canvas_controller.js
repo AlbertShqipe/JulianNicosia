03d2ad9706bf8c166e1007f7e6a777edd0978693
Albert Nikolli <albert_nikolli@icloud.com>
2024-11-07 18:15:22 +0100

    added the dusk part with as well settin the message of website in construction

--- app/javascript/controllers/off_canvas_controller.js
+++ app/javascript/controllers/off_canvas_controller.js
@@ -14,7 +14,6 @@ export default class extends Controller {
       // document.getElementById('text-info-2-phone').style = 'display: none ';
       // document.getElementById('text-info-3-phone').style = 'display: none ';
     });
-
     document.getElementById("closebtn-itii").addEventListener("click", () => {
       document.getElementById("main-itii").style = "display: block";
       document.getElementById("mySidenav-itii").style.width = "0";
@@ -27,6 +26,7 @@ export default class extends Controller {
       // document.getElementById('text-info-3-phone').style = 'display: block';
     });
 
+
     document.getElementById("main-lh").addEventListener("click", () => {
       document.getElementById("main-lh").style = "display: none";
       document.getElementById("mySidenav-lh").style.width = "100%";
@@ -34,11 +34,7 @@ export default class extends Controller {
       document.getElementById('phone-next').style = 'display: none';
       document.getElementById('logo-text').style = "display: none";
       document.getElementById('burger-menu').style = "display: none";
-      // document.getElementById('text-info-1-phone').style = 'display: none ';
-      // document.getElementById('text-info-2-phone').style = 'display: none ';
-      // document.getElementById('text-info-3-phone').style = 'display: none ';
     });
-
     document.getElementById("closebtn-lh").addEventListener("click", () => {
       document.getElementById("main-lh").style = "display: block";
       document.getElementById("mySidenav-lh").style.width = "0";
@@ -46,10 +42,8 @@ export default class extends Controller {
       document.getElementById('phone-next').style = 'display: inline-block;color:red';
       document.getElementById('logo-text').style = "display: inline-block;color:red";
       document.getElementById('burger-menu').style = "display: flex";
-      // document.getElementById('text-info-1-phone').style = 'display: block';
-      // document.getElementById('text-info-2-phone').style = 'display: block';
-      // document.getElementById('text-info-3-phone').style = 'display: block';
     });
+
 
     document.getElementById("main-fot").addEventListener("click", () => {
       document.getElementById("main-fot").style = "display: none";
@@ -58,9 +52,6 @@ export default class extends Controller {
       document.getElementById('phone-next').style = 'display: none';
       document.getElementById('logo-text').style = "display: none";
       document.getElementById('burger-menu').style = "display: none";
-      // document.getElementById('text-info-1-phone').style = 'display: none ';
-      // document.getElementById('text-info-2-phone').style = 'display: none ';
-      // document.getElementById('text-info-3-phone').style = 'display: none ';
     });
     document.getElementById("closebtn-fot").addEventListener("click", () => {
       document.getElementById("main-fot").style = "display: block";
@@ -69,9 +60,27 @@ export default class extends Controller {
       document.getElementById('phone-next').style = 'display: inline-block; color:violet';
       document.getElementById('logo-text').style = "display: inline-block";
       document.getElementById('burger-menu').style = "display: flex";
-      // document.getElementById('text-info-1-phone').style = 'display: block';
-      // document.getElementById('text-info-2-phone').style = 'display: block';
-      // document.getElementById('text-info-3-phone').style = 'display: block';
+    });
+
+
+    document.getElementById("main-d").addEventListener("click", () => {
+      document.getElementById("main-d").style = "display: none";
+      document.getElementById("mySidenav-d").style.width = "100%";
+      document.getElementById('phone-prev').style = 'display: none ';
+      document.getElementById('phone-next').style = 'display: none';
+      document.getElementById('logo-text').style = "display: none";
+      document.getElementById('burger-menu').style = "display: none";
+      // document.getElementById('text-info-1-phone').style = 'display: none ';
+      // document.getElementById('text-info-2-phone').style = 'display: none ';
+      // document.getElementById('text-info-3-phone').style = 'display: none ';
+    });
+    document.getElementById("closebtn-d").addEventListener("click", () => {
+      document.getElementById("main-d").style = "display: block";
+      document.getElementById("mySidenav-d").style.width = "0";
+      document.getElementById('phone-prev').style = 'display: inline-block; color:white ';
+      document.getElementById('phone-next').style = 'display: inline-block; color:white';
+      document.getElementById('logo-text').style = "display: inline-block";
+      document.getElementById('burger-menu').style = "display: flex";
     });
   }
 }
