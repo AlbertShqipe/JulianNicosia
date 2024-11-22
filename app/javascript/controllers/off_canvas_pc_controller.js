4650eb598bf5020e6e2164ebfeb0a8916d89ad01
Albert Nikolli <albert_nikolli@icloud.com>
2024-11-22 18:28:39 +0100

    added the connections to the videos for the computer and the phone as well

---
+++ app/javascript/controllers/off_canvas_pc_controller.js
@@ -0,0 +1,59 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="off-canvas-pc"
+export default class extends Controller {
+  connect() {
+    document.getElementById('text-info-1-pc').addEventListener('click', () => {
+      document.getElementById("burger-menu").style = "opacity: 0";
+      document.getElementById("mySidenav-itii").style.width = "100%";
+      document.getElementById('pc-prev').style = "opacity: 0";
+      document.getElementById('pc-next').style = "opacity: 0";
+    });
+    document.getElementById("closebtn-itii").addEventListener("click", () => {
+      document.getElementById("mySidenav-itii").style.width = "0";
+      document.getElementById("burger-menu").style = "opacity: 1; color: #ceceff";
+      document.getElementById("pc-prev").style = "opacity: 1; color: #ceceff";
+      document.getElementById("pc-next").style = "opacity: 1; color: #ceceff";
+    });
+
+
+    document.getElementById('text-info-2-pc').addEventListener('click', () => {
+      document.getElementById("burger-menu").style = "opacity: 0";
+      document.getElementById("mySidenav-lh").style.width = "100%";
+      document.getElementById('pc-prev').style = "opacity: 0";
+      document.getElementById('pc-next').style = "opacity: 0";
+    });
+    document.getElementById("closebtn-lh").addEventListener("click", () => {
+      document.getElementById("burger-menu").style = "opacity: 1; color: #640000";
+      document.getElementById("mySidenav-lh").style.width = "0";
+      document.getElementById('pc-prev').style = "opacity: 1; color: #640000";
+      document.getElementById('pc-next').style = "opacity: 1; color: #640000";
+    });
+
+    document.getElementById('text-info-3-pc').addEventListener('click', () => {
+      document.getElementById("burger-menu").style = "opacity: 0";
+      document.getElementById("mySidenav-fot").style.width = "100%";
+      document.getElementById('pc-prev').style = "opacity: 0";
+      document.getElementById('pc-next').style = "opacity: 0";
+    });
+    document.getElementById("closebtn-fot").addEventListener("click", () => {
+      document.getElementById("burger-menu").style = "opacity: 1; color: #640a95";
+      document.getElementById("mySidenav-fot").style.width = "0";
+      document.getElementById('pc-prev').style = "opacity: 1; color: #640a95";
+      document.getElementById('pc-next').style = "opacity: 1; color: #640a95";
+    });
+
+    document.getElementById('text-info-5-pc').addEventListener('click', () => {
+      document.getElementById("burger-menu").style = "opacity: 0";
+      document.getElementById("mySidenav-d").style.width = "100%";
+      document.getElementById('pc-prev').style = "opacity: 0";
+      document.getElementById('pc-next').style = "opacity: 0";
+    });
+    document.getElementById("closebtn-d").addEventListener("click", () => {
+      document.getElementById("burger-menu").style = "opacity: 1; color:#898c5e";
+      document.getElementById("mySidenav-d").style.width = "0";
+      document.getElementById('pc-prev').style = "opacity: 1; color:#898c5e";
+      document.getElementById('pc-next').style = "opacity: 1; color:#898c5e";
+    });
+  }
+}
