716df1555a8d7736225b8f7cf4e582978b393e52
Albert Nikolli <albert_nikolli@icloud.com>
2024-12-07 18:05:25 +0100

    took care of the index page and other menu issues

--- app/javascript/controllers/plus_controller.js
+++ app/javascript/controllers/plus_controller.js
@@ -5,12 +5,15 @@ export default class extends Controller {
   connect() {
     const plus = document.getElementById('plus-btn');
     const ulWorks = document.getElementById('works');
+    const worksUl = document.getElementById('works-ul');
     plus.addEventListener('click', () => {
       plus.classList.toggle('turn');
       if (plus.classList.contains('turn')) {
         ulWorks.style = 'display: block';
+        worksUl.style = 'border-bottom: 1px solid rgba(255, 255, 255, 0.75)';
       } else {
         ulWorks.style = 'display: none';
+        worksUl.style = 'border: none';
       }
     });
   }
--- app/javascript/controllers/works_list_disappear_controller.js
+++ app/javascript/controllers/works_list_disappear_controller.js
@@ -3,11 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="works-list-disappear"
 export default class extends Controller {
   connect() {
-    const list = document.getElementById('works-list');
+    // const list = document.getElementById('works-list');
+    // if (window.innerWidth < 760) {
+    //   list.style.display = 'none';
+    // } else {
+    //   list.style.display = 'block';
+    // };
+    const plus = document.getElementById('plus-btn');
     if (window.innerWidth < 760) {
-      list.style.display = 'none';
+      plus.style.display = 'none';
     } else {
-      list.style.display = 'block';
+      plus.style.display = 'inline-block';
     };
   }
 }
