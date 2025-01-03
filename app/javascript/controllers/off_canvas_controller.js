e3674febb2f25fec3eb9778501e17e75327276e2
Albert Nikolli <albert_nikolli@icloud.com>
2025-01-03 22:27:19 +0100

    trying out the swipe to close the sidenav

--- app/javascript/controllers/off_canvas_controller.js
+++ app/javascript/controllers/off_canvas_controller.js
@@ -21,6 +21,17 @@ export default class extends Controller {
     const closeSidenav = (sidenav) => {
       sidenav.style.width = "0";
       sidenav.style.opacity = "0"; // Optional opacity reset
+      document.getElementById('h3-swipe').style = "opacity: 1";
+      document.getElementById("burger-menu").style = "opacity: 1";
+      document.getElementById("main-itii").style = "opacity: 1";
+      document.getElementById("main-lh").style = "opacity: 1";
+      document.getElementById("main-fot").style = "opacity: 1";
+      document.getElementById("main-d").style = "opacity: 1";
+      document.getElementById("main-r-f-f").style = "opacity: 1";
+      document.getElementById("main-ofri").style = "opacity: 1";
+      document.getElementById("main-po").style = "opacity: 1";
+      document.getElementById("main-vop").style = "opacity: 1";
+
     };
 
     // Function to handle swipe events for each sidenav
@@ -38,17 +49,6 @@ export default class extends Controller {
         // Close sidenav if swiped left (swipeDistance > 50) or swiped right (swipeDistance < -50)
         if (swipeDistance > 50 || swipeDistance < -50) {
           closeSidenav(sidenav); // Close the sidenav
-          document.getElementById('h3-swipe').style = "opacity: 1";
-          document.getElementById("burger-menu").style = "opacity: 1";
-          document.getElementById("main-itii").style = "opacity: 1";
-          document.getElementById("main-lh").style = "opacity: 1";
-          document.getElementById("main-fot").style = "opacity: 1";
-          document.getElementById("main-d").style = "opacity: 1";
-          document.getElementById("main-r-f-f").style = "opacity: 1";
-          document.getElementById("main-ofri").style = "opacity: 1";
-          document.getElementById("main-po").style = "opacity: 1";
-          document.getElementById("main-vop").style = "opacity: 1";
-
 
         }
       });
@@ -83,8 +83,8 @@ export default class extends Controller {
       document.getElementById('h3-swipe').style = "opacity: 0";
       document.getElementById("burger-menu").style = "opacity: 0";
       document.getElementById("main-itii").style = "opacity: 0";
-      document.getElementById('phone-prev').style = "opacity: 0";
-      document.getElementById('phone-next').style = "opacity: 0";
+      // document.getElementById('phone-prev').style = "opacity: 0";
+      // document.getElementById('phone-next').style = "opacity: 0";
       openSidenav(sidenavs.itii);
     });
     // Close button handler for sidenav
@@ -92,8 +92,8 @@ export default class extends Controller {
       document.getElementById('h3-swipe').style = "opacity: 1";
       document.getElementById("burger-menu").style = "opacity: 1; color: #2C3651";
       document.getElementById("main-itii").style = " color: #2C3651; transform:translate(-75%,-48%)";
-      document.getElementById('phone-prev').style = "color: #2C3651";
-      document.getElementById('phone-next').style = "color: #2C3651";
+      // document.getElementById('phone-prev').style = "color: #2C3651";
+      // document.getElementById('phone-next').style = "color: #2C3651";
       closeSidenav(sidenavs.itii);
     });
 
@@ -101,31 +101,36 @@ export default class extends Controller {
     document.getElementById("main-lh").addEventListener("click", () => {
       document.getElementById("burger-menu").style = "opacity: 0";
       document.getElementById("main-lh").style = "opacity: 0";
+      openSidenav(sidenavs.lh);
       document.getElementById("mySidenav-lh").style.width = "100%";
-      document.getElementById('phone-prev').style = "opacity: 0";
-      document.getElementById('phone-next').style = "opacity: 0";
+      // document.getElementById('phone-prev').style = "opacity: 0";
+      // document.getElementById('phone-next').style = "opacity: 0";
     });
     document.getElementById("closebtn-lh").addEventListener("click", () => {
       document.getElementById("burger-menu").style = "opacity: 1; color:rgb(100, 0, 0)";
       document.getElementById("main-lh").style ="opacity: 1; color:rgb(100, 0, 0); transform:translate(-57%,-48%)";
       document.getElementById("mySidenav-lh").style.width = "0";
-      document.getElementById('phone-prev').style = "opacity: 1; color:rgb(100, 0, 0)";
-      document.getElementById('phone-next').style = "opacity: 1; color:rgb(100, 0, 0)";
+      // document.getElementById('phone-prev').style = "opacity: 1; color:rgb(100, 0, 0)";
+      // document.getElementById('phone-next').style = "opacity: 1; color:rgb(100, 0, 0)";
+      closeSidenav(sidenavs.lh);
     });
 
     document.getElementById("main-fot").addEventListener("click", () => {
       document.getElementById("burger-menu").style = "opacity: 0";
       document.getElementById("main-fot").style = "opacity: 0";
+
       document.getElementById("mySidenav-fot").style.width = "100%";
-      document.getElementById('phone-prev').style = "opacity: 0";
-      document.getElementById('phone-next').style = "opacity: 0";
+      openSidenav(sidenavs.fot);
+      // document.getElementById('phone-prev').style = "opacity: 0";
+      // document.getElementById('phone-next').style = "opacity: 0";
     });
     document.getElementById("closebtn-fot").addEventListener("click", () => {
       document.getElementById("burger-menu").style = "opacity: 1; color:rgb(100, 10, 149)";
       document.getElementById("main-fot").style = "opacity: 1; color:rgb(100, 10, 149); transform:translate(-54%,-48%)";
       document.getElementById("mySidenav-fot").style.width = "0";
-      document.getElementById('phone-prev').style = "opacity: 1; color:rgb(100, 10, 149)";
-      document.getElementById('phone-next').style = "opacity: 1; color:rgb(100, 10, 149)";
+      closeSidenav(sidenavs.fot);
+      // document.getElementById('phone-prev').style = "opacity: 1; color:rgb(100, 10, 149)";
+      // document.getElementById('phone-next').style = "opacity: 1; color:rgb(100, 10, 149)";
     });
 
 
@@ -133,75 +138,85 @@ export default class extends Controller {
       document.getElementById("burger-menu").style = "opacity: 0";
       document.getElementById("main-d").style = "opacity: 0";
       document.getElementById("mySidenav-d").style.width = "100%";
-      document.getElementById('phone-prev').style = "opacity: 0";
-      document.getElementById('phone-next').style = "opacity: 0";
+      // document.getElementById('phone-prev').style = "opacity: 0";
+      // document.getElementById('phone-next').style = "opacity: 0";
+      openSidenav(sidenavs.d);
     });
     document.getElementById("closebtn-d").addEventListener("click", () => {
       document.getElementById("burger-menu").style = "opacity: 1;  color:#898c5e";
       document.getElementById("main-d").style = "opacity: 1;  color:#898c5e; transform:translate(-60%,-75%)";
       document.getElementById("mySidenav-d").style.width = "0";
-      document.getElementById('phone-prev').style = "opacity: 1; color:#898c5e";
-      document.getElementById('phone-next').style = "opacity: 1; color:#898c5e";
+      // document.getElementById('phone-prev').style = "opacity: 1; color:#898c5e";
+      // document.getElementById('phone-next').style = "opacity: 1; color:#898c5e";
+      closeSidenav(sidenavs.d);
     });
 
     document.getElementById("main-r-f-f").addEventListener("click", () => {
       document.getElementById("burger-menu").style = "opacity: 0";
       document.getElementById("main-r-f-f").style = "opacity: 0";
       document.getElementById("mySidenav-rff").style.width = "100%";
-      document.getElementById('phone-prev').style = "opacity: 0";
-      document.getElementById('phone-next').style = "opacity: 0";
+      // document.getElementById('phone-prev').style = "opacity: 0";
+      // document.getElementById('phone-next').style = "opacity: 0";
+      openSidenav(sidenavs.rff);
     });
     document.getElementById("closebtn-rff").addEventListener("click", () => {
       document.getElementById("mySidenav-rff").style.width = "0";
       document.getElementById("burger-menu").style = "opacity: 1; color:black";
       document.getElementById("main-r-f-f").style = "opacity: 1; color:black; transform:translate(-65%,-62%)";
-      document.getElementById('phone-prev').style = "opacity: 1; color:black";
-      document.getElementById('phone-next').style = "opacity: 1; color:black";
+      // document.getElementById('phone-prev').style = "opacity: 1; color:black";
+      // document.getElementById('phone-next').style = "opacity: 1; color:black";
+      closeSidenav(sidenavs.rff);
     });
 
     document.getElementById("main-ofri").addEventListener("click", () => {
       document.getElementById("burger-menu").style = "opacity: 0";
       document.getElementById("main-ofri").style = "opacity: 0";
       document.getElementById("mySidenav-ofri").style.width = "100%";
-      document.getElementById('phone-prev').style = "opacity: 0";
-      document.getElementById('phone-next').style = "opacity: 0";
+      // document.getElementById('phone-prev').style = "opacity: 0";
+      // document.getElementById('phone-next').style = "opacity: 0";
+      openSidenav(sidenavs.ofri);
     });
     document.getElementById("closebtn-ofri").addEventListener("click", () => {
       document.getElementById("mySidenav-ofri").style.width = "0";
       document.getElementById("burger-menu").style = "opacity: 1; color:#b0885e";
       document.getElementById("main-ofri").style = "opacity: 1; color:#b0885e; transform:translate(-55%,-48%)";
-      document.getElementById('phone-prev').style = "opacity: 1; color:#b0885e";
-      document.getElementById('phone-next').style = "opacity: 1; color:#b0885e";
+      // document.getElementById('phone-prev').style = "opacity: 1; color:#b0885e";
+      // document.getElementById('phone-next').style = "opacity: 1; color:#b0885e";
+      closeSidenav(sidenavs.ofri);
     });
 
     document.getElementById("main-po").addEventListener("click", () => {
       document.getElementById("burger-menu").style = "opacity: 0";
       document.getElementById("main-po").style = "opacity: 0";
       document.getElementById("mySidenav-po").style.width = "100%";
-      document.getElementById('phone-prev').style = "opacity: 0";
-      document.getElementById('phone-next').style = "opacity: 0";
+      // document.getElementById('phone-prev').style = "opacity: 0";
+      // document.getElementById('phone-next').style = "opacity: 0";
+      openSidenav(sidenavs.po);
     });
     document.getElementById("closebtn-po").addEventListener("click", () => {
       document.getElementById("mySidenav-po").style.width = "0";
       document.getElementById("burger-menu").style = "opacity: 1; color:#4871bc";
       document.getElementById("main-po").style = "opacity: 1; color:#4871bc";
-      document.getElementById('phone-prev').style = "opacity: 1; color:#4871bc";
-      document.getElementById('phone-next').style = "opacity: 1; color:#4871bc";
+      // document.getElementById('phone-prev').style = "opacity: 1; color:#4871bc";
+      // document.getElementById('phone-next').style = "opacity: 1; color:#4871bc";
+      closeSidenav(sidenavs.po);
     });
 
     document.getElementById("main-vop").addEventListener("click", () => {
       document.getElementById("burger-menu").style = "opacity: 0";
       document.getElementById("main-vop").style = "opacity: 0";
       document.getElementById("mySidenav-vop").style.width = "100%";
-      document.getElementById('phone-prev').style = "opacity: 0";
-      document.getElementById('phone-next').style = "opacity: 0";
+      // document.getElementById('phone-prev').style = "opacity: 0";
+      // document.getElementById('phone-next').style = "opacity: 0";
+      openSidenav(sidenavs.vop);
     });
     document.getElementById("closebtn-vop").addEventListener("click", () => {
       document.getElementById("mySidenav-vop").style.width = "0";
       document.getElementById("burger-menu").style = "opacity: 1; color:#4871bc";
       document.getElementById("main-vop").style = "opacity: 1; color:#4871bc; transform:translate(-57%,-48%)";
-      document.getElementById('phone-prev').style = "opacity: 1; color:#4871bc";
-      document.getElementById('phone-next').style = "opacity: 1; color:#4871bc";
+      // document.getElementById('phone-prev').style = "opacity: 1; color:#4871bc";
+      // document.getElementById('phone-next').style = "opacity: 1; color:#4871bc";
+      closeSidenav(sidenavs.vop);
     });
   }
 }
