ca164cbfa62c4b16a04a6c32f4c3fd0bbf46a559
Albert Nikolli <albert_nikolli@icloud.com>
2025-01-24 11:46:37 +0100

    added the code, need the testing

---
+++ app/javascript/swipe_back.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.innerWidth < 760) {
+    let touchStartX = 0;
+    let touchEndX = 0;
+
+    document.addEventListener('touchstart', (e) => {
+      touchStartX = e.changedTouches[0].screenX;
+    });
+
+    document.addEventListener('touchend', (e) => {
+      touchEndX = e.changedTouches[0].screenX;
+      handleSwipe();
+    });
+
+    function handleSwipe() {
+      const swipeDistance = touchEndX - touchStartX;
+      if (swipeDistance > 50) { // Swipe right
+        window.history.back();
+      }
+    }
+  }
+});
