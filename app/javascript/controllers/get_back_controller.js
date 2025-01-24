15d0c45ae09cf5d0da98b341d1501be50f8400dd
Albert Nikolli <albert_nikolli@icloud.com>
2025-01-24 12:41:01 +0100

    repairing the swipe back, to still test

--- app/javascript/controllers/get_back_controller.js
+++ app/javascript/controllers/get_back_controller.js
@@ -3,38 +3,39 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="get-back"
 export default class extends Controller {
   connect() {
-    if (window.innerWidth < 760 && window.location.pathname !== '/home') {
-      this.setupSwipeListener();
-    }
+    setupSwipeListener();
   }
 
   setupSwipeListener() {
-    let touchStartX = 0;
-    let touchEndX = 0;
+    if (window.innerWidth < 760 && window.location.pathname !== '/home') {
+      let touchStartX = 0;
+      let touchEndX = 0;
+      const backElements = document.querySelectorAll('.backElement');
 
-    const handleSwipe = () => {
-      const swipeDistance = touchEndX - touchStartX;
-      if (swipeDistance > 50) { // Swipe right
-        window.history.back();
-      }
-    };
+      const handleSwipe = () => {
+        const swipeDistance = touchEndX - touchStartX;
+        if (swipeDistance > 50) { // Swipe right
+          window.history.back();
+        }
+      };
 
-    const onTouchStart = (e) => {
-      touchStartX = e.changedTouches[0].screenX;
-    };
+      const onTouchStart = (e) => {
+        touchStartX = e.changedTouches[0].screenX;
+      };
 
-    const onTouchEnd = (e) => {
-      touchEndX = e.changedTouches[0].screenX;
-      handleSwipe();
-    };
+      const onTouchEnd = (e) => {
+        touchEndX = e.changedTouches[0].screenX;
+        handleSwipe();
+      };
 
-    document.addEventListener('touchstart', onTouchStart);
-    document.addEventListener('touchend', onTouchEnd);
+      document.addEventListener('touchstart', onTouchStart);
+      document.addEventListener('touchend', onTouchEnd);
 
-    // Optionally remove event listeners on disconnect to avoid memory leaks
-    this.disconnect = () => {
-      document.removeEventListener('touchstart', onTouchStart);
-      document.removeEventListener('touchend', onTouchEnd);
-    };
+      // Optionally remove event listeners on disconnect to avoid memory leaks
+      this.disconnect = () => {
+        document.removeEventListener('touchstart', onTouchStart);
+        document.removeEventListener('touchend', onTouchEnd);
+      };
+    }
   }
 }
